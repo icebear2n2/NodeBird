3f089524b52291052845673679b7dad939ef103b
icebear2n2 <icebear2n2@gmail.com>
2023-04-02 18:21:45 +0900

    Model: Hashtag

--- models/hashtag.js
+++ models/hashtag.js
@@ -1,24 +1,21 @@
 const Sequelize = require("sequelize");
 
-module.exports = class Post extends Sequelize.Model {
+module.exports = class Hashtag extends Sequelize.Model {
   static init(sequelize) {
     return super.init(
       {
-        content: {
-          type: Sequelize.STRING(140),
+        title: {
+          type: Sequelize.STRING(15),
           allowNull: false,
-        },
-        img: {
-          type: Sequelize.STRING(200),
-          allowNull: true,
+          unique: true,
         },
       },
       {
         sequelize,
         timestamps: true,
         underscored: false,
-        modelName: "Post",
-        tableName: "posts",
+        modelName: "Hashtag",
+        tableName: "hashtags",
         paranoid: false,
         charset: "utf8mb4",
         collate: "utf8mb4_general_ci",
